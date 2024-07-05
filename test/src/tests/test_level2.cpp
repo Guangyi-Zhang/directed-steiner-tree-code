b9011ae45f05f520fd1b141af76f9f289768d0ed
Guangyi <guangyi.z@outlook.com>
2024-07-05 20:18:07 +0800

    doctest minor

---
+++ test/src/tests/test_level2.cpp
@@ -0,0 +1,188 @@
+#include <fmt/ranges.h>
+#include "dst/dst.hpp"
+#include "dst/dijkstra.hpp"
+#include "dst/utils.hpp"
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <tuple>
+
+
+TEST_CASE("level2_co_alg") {
+  using namespace dst;
+  /*
+    <-----+0+----->
+    |             |
+    |             |
+    v             v
+    1             2+------>
+    +             +       |
+    |             |       |
+    |             |       v
+    v----->11<----v       12
+  */
+
+  std::vector<std::pair<int,int>> edges {std::make_pair(0,1), 
+                                         std::make_pair(0,2), 
+                                         std::make_pair(1,11), 
+                                         std::make_pair(2,11), 
+                                         std::make_pair(2,12), 
+                                         };
+  std::vector<double> weights {1,1, 99,1,1};
+  int root = 0;
+  std::vector<int> terms {11,12};
+  DST dt = DST(edges, weights, root, terms);
+
+  CHECK(dt.naive_alg().cost == 3);
+  CHECK(dt.naive_alg().cost_sc == 4);
+  CHECK(dt.naive_alg().debuginfo.at("sssp_nodes_visited") == "7"); // 5+2 dummies
+
+  CHECK(dt.level2_alg().cost == 3);
+  CHECK(dt.level2_alg().cost_sc == 3);
+  CHECK(dt.level2_alg().debuginfo.at("sssp_nodes_visited") == "16"); // 7+(5+4)
+
+  CHECK(dt.level2_co_alg().cost == 3);
+  CHECK(dt.level2_co_alg().cost_sc == 3);
+  CHECK(dt.level2_co_alg().debuginfo.at("sssp_nodes_visited") == "13"); // 7+2*(3)
+
+  dt.level2_co_alg().print();
+}
+
+
+
+TEST_CASE("Level2PartialTree") {
+  using namespace dst;
+
+  int root {0}, v {1};
+  double d_rv {1};
+  int k = 5;
+  Level2PartialTree tree {root, v, d_rv};
+  CHECK(not tree.is_ready());
+  CHECK(tree.density() > 1e8);
+  CHECK(tree.density_LB(k) < 0);
+
+  tree.add_term(11, 1); // density = 2/1
+  CHECK(eq(tree.density_LB(k), (1.0+k)/k));
+  tree.add_term(12, 2); // density = 4/2
+  CHECK(not tree.is_ready());
+  CHECK(eq(tree.density_LB(k), (1.0+1+(k-1)*2)/k));
+  tree.add_term(13, 3); // density = 7/3
+  CHECK(tree.is_ready());
+  CHECK(eq(tree.density(), 2));
+  CHECK(eq(tree.density_LB(k), tree.density()));
+  CHECK(tree.terms.size() == 2);
+
+  tree.erase_and_reset({11, 12});
+  CHECK(not tree.is_ready());
+  CHECK(eq(tree.density(), 1+3));
+  CHECK(eq(tree.density_LB(k), (1.0+k*3)/k));
+  CHECK(tree.terms.size() == 1);
+
+  tree.add_term(14, 4); // density = (1+3+4)/2
+  CHECK(eq(tree.density_LB(k), (1.0+3+(k-1)*4)/k));
+  tree.add_term(14, 5); // density = (1+3+4+5)/3
+  CHECK(tree.is_ready());
+  CHECK(eq(tree.density(), 4));
+  CHECK(eq(tree.density_LB(k), tree.density()));
+  CHECK(tree.terms.size() == 2);
+
+  Level2PartialTree tree2 {0.5};
+  CHECK(eq(tree2.density(), 0.5));
+}
+
+
+TEST_CASE("crossing") {
+  using namespace dst;
+  /*
+    <---------+0+------->
+    |                   |
+    |                   |
+    v                   v
+    1+-------->11+----->2
+    +                  ++
+    |                  ||
+    v                  +v
+    21         22<-----+23
+  */
+
+  std::vector<std::pair<int,int>> edges {std::make_pair(0,1), 
+                                         std::make_pair(0,2), 
+                                         std::make_pair(1,11), 
+                                         std::make_pair(11,2),
+                                         std::make_pair(1,21), 
+                                         std::make_pair(2,22), 
+                                         std::make_pair(2,23)
+                                         };
+  std::vector<double> weights {1,1, 0.2,0.2, 3,3,1};
+  std::vector<int> terms {21,22,23};
+  DST dt = DST(edges, weights, 0, terms);
+
+  CHECK(dt.naive_alg().cost == 4+4+2-1);
+  CHECK(dt.naive_alg().cost_sc == 4+4+2);
+  auto tree = dt.level2_alg();
+  CHECK(tree.cost_sc == 2+(1+3+0.2*2+3));
+  CHECK(std::abs(tree.cost - tree.cost_sc) < EPSILON);
+  CHECK(std::abs(tree.cost_trimmed() - (tree.cost_sc-0.2*2)) < EPSILON);
+  CHECK((tree.terms_cov == std::unordered_set<int> {24,25,26}));
+}
+
+
+TEST_CASE("cycles") {
+  using namespace dst;
+
+  std::vector<std::pair<int,int>> edges {std::make_pair(0,1), 
+                                         std::make_pair(1,2), 
+                                         std::make_pair(2,3), 
+                                         std::make_pair(3,4), 
+                                         std::make_pair(4,5),
+                                         std::make_pair(5,0)};
+  std::vector<double> weights {1,1,1,1,1,1};
+  std::vector<int> terms {2,4};
+  DST dt = DST(edges, weights, 0, terms);
+
+  CHECK(dt.naive_alg().cost_sc == 2+4);
+  CHECK(dt.naive_alg().cost == 2+2);
+  auto tree = dt.level2_alg();
+  CHECK(tree.cost == 2+2);
+  // CHECK(tree.cost_sc == 2+4); // pick 2 and then 4
+  CHECK(tree.cost_sc == 4); // pick 2 only
+  CHECK((tree.terms_cov == std::unordered_set<int> {6,7}));
+}
+
+
+TEST_CASE("level2_alg") {
+  using namespace dst;
+
+  std::vector<std::pair<int,int>> edges {std::make_pair(0,1), 
+                                         std::make_pair(0,2), 
+                                         std::make_pair(0,3), 
+                                         std::make_pair(0,4), 
+                                         std::make_pair(1,11), 
+                                         std::make_pair(2,12), 
+                                         std::make_pair(3,13), 
+                                         std::make_pair(4,11), 
+                                         std::make_pair(4,12), 
+                                         std::make_pair(4,13)};
+  std::vector<double> weights {1,1,1,2.5, 2,2,2, 1,1,1};
+  std::vector<int> terms {11,12,13};
+  DST dt = DST(edges, weights, 0, terms);
+
+  CHECK(dt.naive_alg().cost == 1*3 + 2*3);
+  CHECK(dt.naive_alg().cost_sc == 1*3 + 2*3);
+
+  auto tree = dt.level2_alg();
+  CHECK(tree.cost == 2.5 + 1*3);
+  CHECK(tree.cost_sc == 2.5 + 1*3);
+  CHECK((tree.terms_cov == std::unordered_set<int> {14,15,16}));
+
+  tree = dt.level2_co_alg();
+  CHECK(tree.cost == 2.5 + 1*3);
+  CHECK(tree.cost_sc == 2.5 + 1*3);
+  CHECK((tree.terms_cov == std::unordered_set<int> {14,15,16}));
+}
+
---
+++ test/src/tests/test_level3.cpp
@@ -0,0 +1,65 @@
+#include <fmt/ranges.h>
+#include "dst/dst.hpp"
+#include "dst/dijkstra.hpp"
+#include "dst/utils.hpp"
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <tuple>
+
+
+TEST_CASE("level3_alg") {
+  using namespace dst;
+  /*
+   +------0-------+    
+   |      +       |    
+   1    +-3-+     2    
+   |\   |   |    /|    
+   | \ 11   12  | |    
+   |  \/|    |\ / |    
+   |  /\|    | \  |    
+   | -  -    || \ |    
+   21   22   23  -24  
+  */
+
+  // r - u - v - {t}
+  // 4 + 2*1 + 4*1 = 10
+  // (2 + 2*2) * 2 = 12
+
+  std::vector<std::pair<int,int>> edges {std::make_pair(0,1), 
+                                         std::make_pair(0,2), 
+                                         std::make_pair(0,3), 
+                                         std::make_pair(1,21), 
+                                         std::make_pair(1,22), 
+                                         std::make_pair(2,23), 
+                                         std::make_pair(2,24), 
+                                         std::make_pair(3,11), 
+                                         std::make_pair(3,12), 
+                                         std::make_pair(11,21), 
+                                         std::make_pair(11,22), 
+                                         std::make_pair(12,23), 
+                                         std::make_pair(12,24)};
+  std::vector<double> weights {2,2,4, 2,2,2,2, 1.5,1.5, 1,1,1,1};
+  std::vector<int> terms {21,22,23,24};
+  DST dt = DST(edges, weights, 0, terms);
+
+  auto &&tree_naive = dt.naive_alg();
+  CHECK(tree_naive.cost_sc == (2+2) * 4);
+  CHECK(tree_naive.cost == (2 + 2*2) * 2);
+
+  auto &&tree2 = dt.level2_alg();
+  CHECK(tree2.cost == (2 + 2*2) * 2);
+  CHECK(tree2.cost_sc == (2 + 2*2) * 2);
+  CHECK((tree2.terms_cov == std::unordered_set<int> {25,26,27,28}));
+
+  auto &&tree3 = dt.level3_alg(0.99);
+  CHECK(tree3.cost == 4 + 2*1.5 + 4*1);
+  CHECK(tree3.cost_sc == 4 + 2*1.5 + 4*1);
+  CHECK((tree3.terms_cov == std::unordered_set<int> {25,26,27,28}));
+}
+
